56a67cc0f242f1dfaef987dd41e7c43dc4f01a10
xzhou6 <37641599+xzhou6@users.noreply.github.com>
2019-04-26 21:19:27 -0400

    Add files via upload

---
+++ scratch.py
@@ -0,0 +1,23 @@
+# Import libraries necessary for this project
+import numpy as np
+import pandas as pd
+from sklearn.cross_validation import ShuffleSplit
+
+# Import supplementary visualizations code visuals.py
+import visuals as vs
+
+# Pretty display for notebooks
+%matplotlib inline
+
+# Load the Boston housing dataset
+data = pd.read_csv('housing.csv')
+prices = data['MEDV']
+features = data.drop('MEDV', axis = 1)
+    
+# Success
+print("Boston housing dataset has {} data points with {} variables each.".format(*data.shape))
+
+# Show data types for all variables
+data.info()
+print('_'*40)
+
